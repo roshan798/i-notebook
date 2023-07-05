a8cea3feb4cc38eed567b88285a1620faf38231f
Roshan <107319136+roshan798@users.noreply.github.com>
2023-07-05 17:07:29 +0530

    Added NoNOtesmessage Component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,18 @@ import React, { useContext } from 'react'
 import AddNotes from './AddNotes'
 import NoteItem from './NoteItem'
 import NoteContext from '../context/notes/noteContext';
+
+const NoNotesMessage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mt-10">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-5a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zm0-6a1 1 0 011 1v2a1 1 0 11-2 0V6a1 1 0 011-1z" clipRule="evenodd" />
+      </svg>
+      <p className="text-gray-500 mt-2">No notes available</p>
+    </div>
+  );
+};
+
 export default function Home() {
   const { notes } = useContext(NoteContext);
   return (
@@ -10,7 +22,7 @@ export default function Home() {
       <div className="container flex flex-col max-w-sm mx-auto my-6">
         <h1 className='text-left text-3xl text-violet-800  mb-3'>Your Notes</h1>
         <div className="flex flex-col justify-center items-center gap-3">
-          {notes.length>0 ? notes.map((note) => <NoteItem notes={note} key={note.id} />) : "Notes not availble"}
+          {(notes && notes.length > 0) ? notes.map((note) => <NoteItem notes={note} key={note.id} />) : <NoNotesMessage/>}
         </div>
       </div>
     </div>
