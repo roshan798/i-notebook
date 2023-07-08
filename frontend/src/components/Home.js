118ae0fdaad76b9595076663593c85bb3cdb61e6
Roshan <107319136+roshan798@users.noreply.github.com>
2023-07-08 21:45:20 +0530

    Center the NoNotes component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,7 +5,7 @@ import NoteContext from '../context/notes/noteContext';
 import LoadingSpinner from './LoadingSpinner'
 const NoNotesMessage = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-10">
+    <div className="flex flex-col items-center justify-center mt-10  sm:col-start-1 sm:col-end-3">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-5a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zm0-6a1 1 0 011 1v2a1 1 0 11-2 0V6a1 1 0 011-1z" clipRule="evenodd" />
       </svg>
