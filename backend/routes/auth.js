b1bec4cbe30df6e904de9151987a5e564f68fcd2
Roshan Kumar <rsahu7989@gmail.com>
2023-10-02 02:40:39 +0530

    reverted to previous version

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,60 +1,61 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const {
-  fetchUser,
-  loginUser,
-  registerUser,
-} = require("../controllers/userController");
-const { body } = require("express-validator");
-const jwt = require("jsonwebtoken");
-const { getUserById } = require("../services/userServices");
+    fetchUser,
+    loginUser,
+    registerUser,
+} = require('../controllers/userController')
+const { body } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { getUserById } = require('../services/userServices')
 
 router.post(
-  "/createUser",
-  [
-    body("name", "Enter a valid name!!").isLength({ min: 3 }),
-    body("password", "Password  must be atleast 8 character").isLength({
-      min: 3,
-    }),
-    body("email", "Enter a valid email!!").isEmail(),
-  ],
-  registerUser,
-  (req, res) => {
-    return res.status(200).send({
-      success: true,
-      message: "User registered successfully",
-    });
-  },
-);
+    '/createUser',
+    [
+        body('name', 'Enter a valid name!!').isLength({ min: 3 }),
+        body('password', 'Password  must be atleast 8 character').isLength({
+            min: 3,
+        }),
+        body('email', 'Enter a valid email!!').isEmail(),
+    ],
+    registerUser,
+    (req, res) => {
+        return res.status(200).send({
+            success: true,
+            message: 'User registered successfully',
+        })
+    }
+)
 
 router.post(
-  "/login",
-  [
-    body("password", "Password  must be atleast 8 character").isLength({
-      min: 3,
-    }),
-    body("email", "Enter a valid email!!").isEmail(),
-  ],
-  loginUser,
-);
+    '/login',
+    [
+        body('password', 'Password  must be atleast 8 character').isLength({
+            min: 3,
+        }),
+        body('email', 'Enter a valid email!!').isEmail(),
+    ],
+    loginUser
+)
 
 //login required
-router.get("/getUser", fetchUser, async (req, res) => {
-  try {
-    const user = {
-      name: req.user.name,
-      email: req.user.email,
-    };
-    return res.send({
-      success: true,
-      user: user,
-    });
-  } catch (error) {
-    res.status(401).json({
-      success: false,
-      error: "internal server error \n ERROR: " + error.message,
-    });
-  }
-});
+router.get('/getUser', fetchUser, async (req, res) => {
+    try {
+        const user = {
+            name: req.user.name,
+            email: req.user.email,
+        }
+        return res.send({
+            success: true,
+            user: user,
+        })
+    } catch (error) {
+        res.status(401).json({
+            success: false,
+            error: 'internal server error \n ERROR: ' + error.message,
+        })
+    }
+})
+router.post('forgot-password', (req, res) => {})
 
-module.exports = router;
+module.exports = router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,37 +12,40 @@ import Notifiction from './components/Notification'
 function App() {
     const { authToken, showAlert } = useContext(UserContext)
     return (
-        <div className="App text-center">
-            <Navbar></Navbar>
-            {showAlert && <Notifiction />}
-            <Routes>
-                <Route
-                    exact
-                    path="/"
-                    element={
-                        authToken ? <Home /> : <Navigate to={'/login'} />
-                    }></Route>
-                <Route
-                    exact
-                    path="/login"
-                    element={
-                        authToken ? <Navigate to={'/'} /> : <Login />
-                    }></Route>
-                <Route
-                    exact
-                    path="/signup"
-                    element={
-                        authToken ? <Navigate to={'/'} /> : <Signup />
-                    }></Route>
-                <Route
-                    exact
-                    path="/note/:id"
-                    element={
-                        authToken ? <Note /> : <Navigate to={'/login'} />
-                    }></Route>
-                <Route exact path="*" element={<Error404 />}></Route>
-            </Routes>
-        </div>
+        <>
+            <div className="App text-center">
+                <Navbar></Navbar>
+                {showAlert && <Notifiction />}
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            authToken ? <Home /> : <Navigate to={'/login'} />
+                        }></Route>
+                    <Route
+                        exact
+                        path="/login"
+                        element={
+                            authToken ? <Navigate to={'/'} /> : <Login />
+                        }></Route>
+                    <Route
+                        exact
+                        path="/signup"
+                        element={
+                            authToken ? <Navigate to={'/'} /> : <Signup />
+                        }></Route>
+                    <Route
+                        exact
+                        path="/note/:id"
+                        element={
+                            authToken ? <Note /> : <Navigate to={'/login'} />
+                        }></Route>
+                    <Route exact path="*" element={<Error404 />}></Route>
+                </Routes>
+            </div>
+            <Footer />
+        </>
     )
 }
 
